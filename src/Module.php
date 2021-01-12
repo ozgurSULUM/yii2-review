98c4741ab3c1297ba7cb77d87a44bb9a056849ae
ozgurSULUM <ozgursulum0@gmail.com>
2021-01-12 20:27:35 +0000

    module initial

---
+++ src/Module.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ozgurSULUM\review;
+
+class Module extends \yii\base\Module
+{
+	public function init()
+	{
+		parent::init();
+	}
+
+
+}
+?>
---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ozgurSULUM\review\controllers;
+
+class DefaultController extends \yii\web\Controller
+{
+	public function actionIndex()
+	{
+		return $this->render('index');
+	}
+}
+?>
